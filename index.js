7a1315c61e16727537198323bd86cbacc9e92c68
Chris Wiegand <chris@wiegandtech.net>
2023-05-14 22:00:26 -0600

    add some very initial model and items - may break

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 (function () {
-  /* hello, world */
+  window.rcr_items = [];
+
+  function pushrcr(image, title, compost, recycle) {
+    window.rcr_items.push({
+      icon: image,
+      title: title,
+      compost_details: compost,
+      recycle_details: recycle,
+    });
+  }
+
+  pushrcr(
+    "fixme",
+    "Cooked Meat",  
+    { capable: true, preferred: true, description: "Note that raw or uncooked meats are NOT compostable." }
+  );
+  pushrcr(
+    "fixme",
+    "Cardboard (clean)",
+    { capable: true, preferred: true, description: "Cardboard must be clean, and not greasy or water-soaked." }
+  );
+  pushrcr(
+    "fixme",
+    "Cardboard (greasy)",
+    { capable: true, preferred: true, description: "Cut or tear it into hand-sized pieces before putting into composting." }
+  );
 })();
